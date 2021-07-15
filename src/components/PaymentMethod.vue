9536bc859d64e46aa6fdae11d5e3a7ac563cd331
Usama Khushnood <usamakhushnood4@gmail.com>
2021-07-15 13:51:27 +0500

    default value checked

--- src/components/PaymentMethod.vue
+++ src/components/PaymentMethod.vue
@@ -10,11 +10,16 @@
         <i :class="x.icon" class="pt-icon"></i>
       </div>
       <div class="col-md-6 payment-method-details">
-        <h6>{{x.title}}</h6>
-        <p class="text-secondary">{{x.desc}}</p>
+        <h6>{{ x.title }}</h6>
+        <p class="text-secondary">{{ x.desc }}</p>
       </div>
       <div class="selected-method col-md-3 text-end">
-        <b-form-radio :id="'checkbox' + xIndex" name="payment-method">
+        <b-form-radio
+          :id="'checkbox' + xIndex"
+          name="payment-method"
+          :value="xIndex"
+          v-model="ptValue"
+        >
         </b-form-radio>
       </div>
     </label>
@@ -24,10 +29,23 @@
 export default {
   data() {
     return {
+      ptValue: 0,
       paymentMethods: [
-        { title: "Cash Payment", desc: "Default Method", icon: 'fa fa-money-bill'},
-        { title: "***********5967", desc: "Expires 09/27", icon: 'fa fa-cc-mastercard' },
-        { title: "***********5784", desc: "Expires 12/25", icon: 'fa fa-cc-visa' },
+        {
+          title: "Cash Payment",
+          desc: "Default Method",
+          icon: "fa fa-money-bill",
+        },
+        {
+          title: "***********5967",
+          desc: "Expires 09/27",
+          icon: "fa fa-cc-mastercard",
+        },
+        {
+          title: "***********5784",
+          desc: "Expires 12/25",
+          icon: "fa fa-cc-visa",
+        },
       ],
     };
   },
@@ -44,6 +62,6 @@ export default {
 }
 
 i.pt-icon {
-    font-size: 30px;
+  font-size: 30px;
 }
 </style>    
